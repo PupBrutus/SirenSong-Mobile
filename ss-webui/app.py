77412f85a59d60cc7191e09a388bf89b01055635
Pup Brutus <PupBrutus@users.noreply.github.com>
2024-11-20 17:32:37 -0800

    Code now dynamically creates controls based on websocket API response. Style/UI updated for better UX

--- ss-webui/app.py
+++ ss-webui/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from jinja2 import TemplateNotFound
 from pythonosc.udp_client import SimpleUDPClient
 import requests
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    server = '127.0.0.1'
+    server = '192.168.1.104'
     port = '1234'
     data_retrieve_port = '5678'
     if request.method == 'POST':
@@ -19,13 +19,17 @@ def index():
                 for key, value in request.form.items():
                     if key not in ['action', 'instanceParam']:
                         client.send_message(instance_param, float(value) if value.replace('.', '', 1).isdigit() else value)
+                print(f"Connected and sent data to {server}:{port}")
                 return '', 204  # No Content response for AJAX request
             except Exception as e:
+                print(f"Failed to connect to server: {e}")
                 return f"Failed to connect to server: {e}", 500
 
     try:
+        print(f"Rendering template with server: {server}, port: {port}, data_retrieve_port: {data_retrieve_port}")
         return render_template('index.html', server=server, port=port, data_retrieve_port=data_retrieve_port)
     except TemplateNotFound:
+        print("Template not found")
         return "Template not found", 404
 
 if __name__ == '__main__':
